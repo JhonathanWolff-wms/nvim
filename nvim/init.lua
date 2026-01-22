5af5766e90548a2421e9842c00e5fbf0b23e43fa
Jhonathan Wolff <jhonathan.wolff@wppmediaservices.com.br>
2026-01-22 15:42:04 -0300

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,3 +17,13 @@ vim.api.nvim_create_autocmd("FileType", {
     pcall(function() vim.treesitter.start() end)
   end,
 })
+
+-- space indentation
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+
+
+
